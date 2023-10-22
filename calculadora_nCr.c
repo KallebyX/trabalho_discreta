51e4883c5ef64c204a83c7f89f683cc21a3e307f
Kalleby Evangelho <82461649+KallebyX@users.noreply.github.com>
2023-10-22 15:17:28 -0300

    criado calculadora_nCr.c
    apenas a calculadora

---
+++ calculadora_nCr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+long long fatorial(int n) {
+    if (n == 0) return 1;
+    long long fat = 1;
+    for (int i = 1; i <= n; i++) {
+        fat *= i;
+    }
+    return fat;
+}
+
+long long combinacao(int n, int r) {
+    return fatorial(n) / (fatorial(r) * fatorial(n - r));
+}
+
+int main() {
+    int n, r;
+    
+    printf("Digite o valor de n: ");
+    scanf("%d", &n);
+    
+    printf("Digite o valor de r: ");
+    scanf("%d", &r);
+    
+    if (r > n) {
+        printf("r não pode ser maior que n.\n");
+        return 1;
+    }
+    
+    printf("nCr = %lld\n", combinacao(n, r));
+    
+    return 0;
+}
